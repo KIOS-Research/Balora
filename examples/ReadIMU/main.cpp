3da308f3a1d84d17bf2bf064bbcae8eae2f1afea
Theodosis Pasiali <pasialis97@icloud.com>
2023-05-26 22:45:08 +0300

    added IMU example

---
+++ examples/ReadIMU/main.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+#include <Balora.h>
+
+Balora node("B1");
+sensors_vec_t accel, gyro;
+
+void setup()
+{
+    Serial.begin(115200);
+    node.begin();
+    node.setLowPowerCPU();
+    while (!Serial.available())
+    {
+        ;
+    }
+    Serial.println("------IMU EXAMPLE------");
+}
+
+void loop()
+{
+    accel = node.getAccel();
+    gyro = node.getGyro();
+
+    Serial.println("-----Accelerometer-----");
+    Serial.println(String("X: " + String(accel.x) + ", Y: " + String(accel.y) + " , Z: " + String(accel.z)));
+
+    Serial.println("-------Gyroscope-------");
+    Serial.println(String("X: " + String(gyro.x) + ", Y: " + String(gyro.y) + " , Z: " + String(gyro.z)));
+
+    node.handleBattery();
+    node.showBatteryState();
+    delay(1000);
+}
